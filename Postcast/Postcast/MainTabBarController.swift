2bbe3b0e2a74ad0702171c1b0f9985726b04464a
liuchialin <charles.ee96@g2.nctu.edu.tw>
2020-05-09 17:18:31 +0800

    set three tabbaritem for controllers

--- Postcast/Postcast/MainTabBarController.swift
+++ Postcast/Postcast/MainTabBarController.swift
@@ -11,8 +11,30 @@ import UIKit
 class MainTabBarController: UITabBarController {
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = .green
-        let favoriteVC = ViewController()
-        viewControllers = [favoriteVC]
+        self.tabBar.tintColor = .purple
+        
+        setupVC()
+        
+    }
+    //MARK:- Setup Function
+    func setupVC(){
+        let favoriteNavVC = generateNavigationVC(with: ViewController(), title: "Favorites", image: UIImage(named: "favorites") ?? UIImage())
+            
+        let searchNavVC = generateNavigationVC(with: ViewController(), title: "Search", image: UIImage(named: "search") ?? UIImage())
+        
+        let downloadNavVC = generateNavigationVC(with: ViewController(), title: "Downloads", image: UIImage(named: "downloads") ?? UIImage())
+        viewControllers = [favoriteNavVC, searchNavVC, downloadNavVC]
+    }
+    //MARK:- Helper Function
+    fileprivate func generateNavigationVC(with rootVC: UIViewController, title: String, image: UIImage) -> UINavigationController {
+        let navVC = UINavigationController(rootViewController: rootVC)
+        navVC.navigationBar.prefersLargeTitles = true
+        //Below will fail the code presentation
+//        navVC.navigationItem.title = title
+        //end
+        rootVC.navigationItem.title = title
+        navVC.tabBarItem.title = title
+        navVC.tabBarItem.image = image
+        return navVC
     }
 }
