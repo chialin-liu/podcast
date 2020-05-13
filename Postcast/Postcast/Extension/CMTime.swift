45dbb6b2b78c46253f765623700b1ffebccc26d7
liuchialin <charles.ee96@g2.nctu.edu.tw>
2020-05-13 21:56:20 +0800

    update current time and duration

---
+++ Postcast/Postcast/Extension/CMTime.swift
@@ -0,0 +1,21 @@
+//
+//  CMTime.swift
+//  Postcast
+//
+//  Created by Chialin Liu on 2020/5/13.
+//  Copyright © 2020 Chialin Liu. All rights reserved.
+//
+
+import Foundation
+import AVKit
+
+extension CMTime {
+    func toDisplayString() -> String {
+        let totalSeconds = Int(CMTimeGetSeconds(self))
+        print("total seconds", totalSeconds)
+        let seconds = totalSeconds % 60
+        let minutes = totalSeconds / 60
+        let timeFormatString = String(format: "%02d:%02d", minutes, seconds)
+        return timeFormatString
+    }
+}
--- Postcast/Postcast/Views/PlayerDetailView.swift
+++ Postcast/Postcast/Views/PlayerDetailView.swift
@@ -33,11 +33,16 @@ class PlayerDetailView: UIView {
         avplayer.automaticallyWaitsToMinimizeStalling = false
         return avplayer
     }()
+    @IBOutlet weak var currentTimeSlider: UISlider!
     @IBOutlet weak var playPauseButton: UIButton! {
         didSet {
             playPauseButton.addTarget(self, action: #selector(handlePlayPause), for: .touchUpInside)
         }
     }
+    @IBAction func currentTimeSlider(_ sender: Any) {
+    }
+    @IBOutlet weak var durationLabel: UILabel!
+    @IBOutlet weak var currentTimeLabel: UILabel!
     @objc func handlePlayPause() {
         if player.timeControlStatus == .paused {
             playPauseButton.setImage(UIImage(named: "pause"), for: .normal)
@@ -51,7 +56,7 @@ class PlayerDetailView: UIView {
     }
     fileprivate func shrinkEpisodeImageView() {
         UIView.animate(withDuration: 0.75, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 1, options: .curveEaseOut, animations: {
-            let scale: CGFloat = 0.7
+            let scale: CGFloat = 0.8
             self.episodeImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
         }, completion: nil)
     }
@@ -65,13 +70,29 @@ class PlayerDetailView: UIView {
         didSet {
             episodeImageView.layer.cornerRadius = 5
             episodeImageView.clipsToBounds = true
-            let scale: CGFloat = 0.7
+            let scale: CGFloat = 0.8
             episodeImageView.transform = CGAffineTransform(scaleX: scale, y: scale)
         }
     }
     //TBD, don't know why..
+    fileprivate func observePlayerCurrentTime() {
+        let interval = CMTimeMake(value: 1, timescale: 2)
+        player.addPeriodicTimeObserver(forInterval: interval, queue: .main) { (time) in
+            self.currentTimeLabel.text = time.toDisplayString()
+            let durationTime = self.player.currentItem?.duration
+            self.durationLabel.text = durationTime?.toDisplayString()
+            self.updateCurrentTimeSlider()
+        }
+    }
+    fileprivate func updateCurrentTimeSlider() {
+        let currentTimeSeconds = CMTimeGetSeconds(player.currentTime())
+        let durationTimeSeconds = CMTimeGetSeconds(player.currentItem?.duration ?? CMTime())
+        let percentage = currentTimeSeconds / durationTimeSeconds
+        self.currentTimeSlider.value = Float(percentage)
+    }
     override func awakeFromNib() {
         super.awakeFromNib()
+        observePlayerCurrentTime()
         let time = CMTimeMake(value: 1, timescale: 3)
         let times = [NSValue(time: time)]
         player.addBoundaryTimeObserver(forTimes: times, queue: .main) {
