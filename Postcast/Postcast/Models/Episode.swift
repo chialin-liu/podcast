372ed464bd1f630d52aa49cd141a32909d0f0310
liuchialin <charles.ee96@g2.nctu.edu.tw>
2020-05-13 13:01:43 +0800

    add avplayer and xib for layout

--- Postcast/Postcast/Models/Episode.swift
+++ Postcast/Postcast/Models/Episode.swift
@@ -12,12 +12,16 @@ struct Episode {
     let title: String
     let pubDate: Date
     let description: String
+    let author: String
+    let streamUrl: String
     //assume the imageUrl will not exist
     var imageUrl: String?
     init(feedItem: RSSFeedItem) {
+        self.streamUrl = feedItem.enclosure?.attributes?.url ?? ""
         self.title = feedItem.title ?? ""
         self.pubDate = feedItem.pubDate ?? Date()
         self.description = feedItem.iTunes?.iTunesSubtitle ??  feedItem.description ?? ""
+        self.author = feedItem.iTunes?.iTunesAuthor ?? ""
         //remove the intial value "" empty string below
         self.imageUrl = feedItem.iTunes?.iTunesImage?.attributes?.href
     }
--- Postcast/Postcast/Views/PlayerDetailView.swift
+++ Postcast/Postcast/Views/PlayerDetailView.swift
@@ -9,15 +9,50 @@
 import Foundation
 import UIKit
 import SDWebImage
+import AVKit
+import AVFoundation
 class PlayerDetailView: UIView {
     var episode: Episode! {
         didSet {
             episodeLabel.text = episode.title
             let url = URL(string: episode.imageUrl?.toSecureHTTPS() ?? "")
             episodeImageView.sd_setImage(with: url, completed: nil)
+            authorLabel.text = episode.author
+            playEpisode()
+            playPauseButton.setImage(UIImage(named: "pause"), for: .normal)
         }
     }
-    @IBOutlet weak var episodeLabel: UILabel!
+    fileprivate func playEpisode() {
+        guard let url = URL(string: episode.streamUrl.toSecureHTTPS()) else { return }
+        let playerItem = AVPlayerItem(url: url)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+    }
+    let player: AVPlayer = {
+        let avplayer = AVPlayer()
+        avplayer.automaticallyWaitsToMinimizeStalling = false
+        return avplayer
+    }()
+    @IBOutlet weak var playPauseButton: UIButton! {
+        didSet {
+            playPauseButton.addTarget(self, action: #selector(handlePlayPause), for: .touchUpInside)
+        }
+    }
+    @objc func handlePlayPause() {
+        if player.timeControlStatus == .paused {
+            playPauseButton.setImage(UIImage(named: "pause"), for: .normal)
+            player.play()
+        } else {
+            playPauseButton.setImage(UIImage(named: "play"), for: .normal)
+            player.pause()
+        }
+    }
+    @IBOutlet weak var authorLabel: UILabel!
+    @IBOutlet weak var episodeLabel: UILabel! {
+        didSet {
+            episodeLabel.numberOfLines = 2
+        }
+    }
     @IBOutlet weak var episodeImageView: UIImageView!
     @IBAction func handleDismiss(_ sender: Any) {
         self.removeFromSuperview()
